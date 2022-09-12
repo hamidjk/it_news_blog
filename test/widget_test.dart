fc7d969d54cec8577498e4f192007872f2f41283
hamidjk <hamidjk.business@gmail.com>
2022-09-13 00:22:38 +0430

    second commit by hamidjk

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,13 +8,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:it_news_blog_hamidjk/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
+    
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
